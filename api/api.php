9c7e3eabd716f3904f85236cadaeac8ee48b94b3
marcelgiglio <marcelgiglio90@gmail.com>
2024-05-19 19:47:06 -0300

    tira um comentário desnecessário

--- api/api.php
+++ api/api.php
@@ -9,7 +9,7 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 if (preg_match('#^/api/channels#', $uri)) {
     require_once __DIR__ . '/channels.php';
 } elseif (preg_match('#^/api/videos#', $uri)) {
-    require_once __DIR__ . '/videos.php'; // Certifique-se de criar este arquivo corretamente mais tarde
+    require_once __DIR__ . '/videos.php';
 } else {
     echo json_encode(['error' => 'Invalid endpoint.']);
     exit;
